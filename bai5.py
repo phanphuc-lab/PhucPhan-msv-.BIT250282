837289844ff18c6cd34c562de1cd7b21729dd277
phanphuc-lab <phanphucxo25@gmail.com>
2026-03-23 15:30:21 +0700

    Add dictionary and key existence check
    Fix key check logic in dictionary lookup

---
+++ bai5.py
@@ -0,0 +1,6 @@
+my_dict = {"tên:hihi", "tuổi:20", "thành phố: Hà Nội"}
+key = input(f"nhập key cần kiểm tra:")
+if key in key:
+    print("key tồn tại trong dictionary")
+else:
+    print("key không tồn tại trong dictionary")
